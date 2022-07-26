d02b2028b4e7f730840d347dc8bf37ad843e3e3b
Murkrow <marco.coppola3@studio.unibo.it>
2022-07-26 19:16:55 +0200

    Cleaning

---
+++ CMakeFiles/GameProject2.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/murkrow/Repos/GameProject/src/Entities/player.cpp" "CMakeFiles/GameProject2.dir/Entities/player.cpp.o" "gcc" "CMakeFiles/GameProject2.dir/Entities/player.cpp.o.d"
+  "/home/murkrow/Repos/GameProject/src/Tools/savedata.cpp" "CMakeFiles/GameProject2.dir/Tools/savedata.cpp.o" "gcc" "CMakeFiles/GameProject2.dir/Tools/savedata.cpp.o.d"
+  "/home/murkrow/Repos/GameProject/src/Tools/utils.cpp" "CMakeFiles/GameProject2.dir/Tools/utils.cpp.o" "gcc" "CMakeFiles/GameProject2.dir/Tools/utils.cpp.o.d"
+  "/home/murkrow/Repos/GameProject/src/UI/alert.cpp" "CMakeFiles/GameProject2.dir/UI/alert.cpp.o" "gcc" "CMakeFiles/GameProject2.dir/UI/alert.cpp.o.d"
+  "/home/murkrow/Repos/GameProject/src/UI/dialog.cpp" "CMakeFiles/GameProject2.dir/UI/dialog.cpp.o" "gcc" "CMakeFiles/GameProject2.dir/UI/dialog.cpp.o.d"
+  "/home/murkrow/Repos/GameProject/src/UI/menu.cpp" "CMakeFiles/GameProject2.dir/UI/menu.cpp.o" "gcc" "CMakeFiles/GameProject2.dir/UI/menu.cpp.o.d"
+  "/home/murkrow/Repos/GameProject/src/UI/message.cpp" "CMakeFiles/GameProject2.dir/UI/message.cpp.o" "gcc" "CMakeFiles/GameProject2.dir/UI/message.cpp.o.d"
+  "/home/murkrow/Repos/GameProject/src/UI/puzzle.cpp" "CMakeFiles/GameProject2.dir/UI/puzzle.cpp.o" "gcc" "CMakeFiles/GameProject2.dir/UI/puzzle.cpp.o.d"
+  "/home/murkrow/Repos/GameProject/src/main.cpp" "CMakeFiles/GameProject2.dir/main.cpp.o" "gcc" "CMakeFiles/GameProject2.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/GameProject2.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameProject2.dir/Entities/player.cpp.o"
+  "CMakeFiles/GameProject2.dir/Entities/player.cpp.o.d"
+  "CMakeFiles/GameProject2.dir/Tools/savedata.cpp.o"
+  "CMakeFiles/GameProject2.dir/Tools/savedata.cpp.o.d"
+  "CMakeFiles/GameProject2.dir/Tools/utils.cpp.o"
+  "CMakeFiles/GameProject2.dir/Tools/utils.cpp.o.d"
+  "CMakeFiles/GameProject2.dir/UI/alert.cpp.o"
+  "CMakeFiles/GameProject2.dir/UI/alert.cpp.o.d"
+  "CMakeFiles/GameProject2.dir/UI/dialog.cpp.o"
+  "CMakeFiles/GameProject2.dir/UI/dialog.cpp.o.d"
+  "CMakeFiles/GameProject2.dir/UI/menu.cpp.o"
+  "CMakeFiles/GameProject2.dir/UI/menu.cpp.o.d"
+  "CMakeFiles/GameProject2.dir/UI/message.cpp.o"
+  "CMakeFiles/GameProject2.dir/UI/message.cpp.o.d"
+  "CMakeFiles/GameProject2.dir/UI/puzzle.cpp.o"
+  "CMakeFiles/GameProject2.dir/UI/puzzle.cpp.o.d"
+  "CMakeFiles/GameProject2.dir/main.cpp.o"
+  "CMakeFiles/GameProject2.dir/main.cpp.o.d"
+  "GameProject2"
+  "GameProject2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameProject2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
