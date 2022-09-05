4da8cf9dea64e3ce2a292c0e0a5cd42f1f3e3ad9
Murkrow <marcocoppola02.mc@gmail.com>
2022-09-05 15:52:07 +0200

    Fixes

--- src/Tools/GameObjectNode.hpp
+++ src/Tools/GameObjectNode.hpp
@@ -2,21 +2,34 @@
 #include "../Entities/entity.hpp"
 
 class GameObject;
+
 struct Node
 {
     GameObject *data;
     Node *next;
 };
+
 class GameObjectNode{   
 
 public:
+
     Node *head; // head is null by default
 
+    // returns (if present) a reference to entity placed at point (x,y)
     Entity* findEntityAtPos(int x, int y, GameObject *excluded);
+
+    // add a new gameobject to list
     void insert(GameObject *item);
+
+    // removes gameobject from list
     void remove(GameObject *item);
+
+    // clears the list
     void reset();
+
+    // returns the total number of enemies in list
     int numberOfEnemies(GameObjectList *gameObjects);
+
     GameObjectNode();
 
 };
